5f0d980be7b525b63053c75786299d0d70091231
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-23 12:42:24 -0600

    Add home.js to declare the  addEventListener for the buttons

---
+++ src/home.js
@@ -0,0 +1,12 @@
+const btnComments = document.getElementById('btnComments');
+const btnReservations = document.getElementById('btnReservations');
+
+btnComments.addEventListener('click', () => {
+  console.log('btnComments');
+});
+
+btnReservations.addEventListener('click', () => {
+  console.log('btnReservations');
+});
+
+export { btnComments, btnReservations };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.css';
+import { btnComments, btnReservations } from './index.mjs';
