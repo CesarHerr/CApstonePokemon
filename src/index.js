8394caeb7ebacffc27e30dfc602d244c282d6fe2
Cesar Herrera <cesarherr321@gmail.com>
2023-05-24 15:38:29 -0400

    Add card styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './style.css';
 import getPokeInfo from './modules/popup.js';
-import getPokemon2 from './modules/button';
 
 getPokeInfo();
-
-getPokemon2();
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,31 +1,11 @@
-const main = document.querySelector("header");
+const main = document.querySelector('header');
 
+const urlAPI = 'https://pokeapi.co/api/v2/pokemon';
 
-const urlAPI = "https://pokeapi.co/api/v2/pokemon";
-
-const getPokemon = async (id) => {
-  try {
-    const response = await fetch(`${urlAPI}/${id}`);
-    const data = await response.json();
-
-    const pokeInfo = {
-      img: data.sprites.other.dream_world.front_default,
-      name: data.forms[0].name[0].toUpperCase() + data.forms[0].name.substring(1),
-      ability: data.abilities[0].ability.name[0].toUpperCase() + data.abilities[0].ability.name.substring(1),
-      weight: (data.weight * 0.1).toFixed(1),
-      height: (data.height * 0.1).toFixed(1),
-      type: data.types[0].type.name[0].toUpperCase() + data.types[0].type.name.substring(1) 
-      
-    };
-    createPopup(pokeInfo);
-  } catch (error) {
-    alert("Error");
-  }
-};
-
+// create popup with API information
 const createPopup = (pokeInfo) => {
-  const cards = document.createElement("div");
-  cards.className = "popup-container";
+  const cards = document.createElement('div');
+  cards.className = 'popup-container';
   cards.innerHTML = `
       <div class="cardPopup">
         <span class="material-symbols-outlined close">close</span>      
@@ -42,24 +22,46 @@ const createPopup = (pokeInfo) => {
         <div class="cardPopup__form">
           <h2>Add a Comment</h2>
           <form>
-            <input type="text" name="username" placeholder="Your Name" required>
-            <textarea id="text-area" placeholder="Your comments" maxlength="400" name="message" required></textarea>
-            <button class="comentBtn">Go</button>
+            <input class='username' type="text" name="username" placeholder="Your Name" required>
+            <textarea class='comment' id="text-area" placeholder="Your comments" maxlength="400" name="message" required></textarea>
+            <button class="commentButton" data-index="${pokeInfo.id}">Go</button>
           </form>
         </div>      
       </div>`;
 
   main.appendChild(cards);
-  const close = document.querySelector(".close");
-  close.addEventListener("click", () => {
+  const close = document.querySelector('.close');
+  close.addEventListener('click', () => {
     main.removeChild(cards);
   });
 };
 
+// fetch API
+const getPokemon = async (id) => {
+  try {
+    const response = await fetch(`${urlAPI}/${id}`);
+    const data = await response.json();
+    const abilities = data.abilities[0];
+    const pokeInfo = {
+      img: data.sprites.other.dream_world.front_default,
+      name: data.forms[0].name[0].toUpperCase() + data.forms[0].name.substring(1),
+      ability: abilities.ability.name[0].toUpperCase() + abilities.ability.name.substring(1),
+      weight: (data.weight * 0.1).toFixed(1),
+      height: (data.height * 0.1).toFixed(1),
+      type: data.types[0].type.name[0].toUpperCase() + data.types[0].type.name.substring(1),
+      id,
+    };
+    createPopup(pokeInfo);
+  } catch (error) {
+    alert('error.message');
+  }
+};
+
+// Show Popup
 const getPokeInfo = () => {
-  document.addEventListener("click", (event) => {
-    if (event.target.classList.contains("seePokemon")) {
-      const index = event.target.dataset.index;
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('seePokemon')) {
+      const { index } = event.target.dataset;
       getPokemon(index);
     }
   });
