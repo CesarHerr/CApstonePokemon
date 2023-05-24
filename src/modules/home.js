226c2dc336ce0aff9467d0b0e7d4ad813f05c7e8
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-24 10:15:23 -0600

    Update home.js

--- src/modules/home.js
+++ src/modules/home.js
@@ -34,40 +34,40 @@
 
 // export default createHome;
 
-const urlAPI = 'https://pokeapi.co/api/v2/pokemon';
+const urlAPI = "https://pokeapi.co/api/v2/pokemon";
 
 const createHome = async () => {
   try {
     const response = await fetch(`${urlAPI}`);
     const data = await response.json();
-    console.log(data.results);
-
-    displayScores(data.results);
+    console.log(data.results)
+    
+    displayScores(data.results)
   } catch (error) {
-    alert('Errooooooor');
+    alert("Errooooooor");
   }
 };
 
 const main = document.querySelector('header');
 const list = document.createElement('div');
 list.classList.add('pokeGroup');
-main.insertAdjacentElement('afterEnd', list);
+main.insertAdjacentElement('afterEnd', list)
 
 const displayScores = (data) => {
-  list.innerHTML = data
-    .map(
-      (data, index) => `
-      <div>
-        <h2>${data.name}</h2>
-        <img class ="indexPokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg"></img>
-        <button class="seePokemon" data-index=${index + 1}>comments</button>
-      </diV>
-    `,
-    )
-    .join('');
-};
+    list.innerHTML = data
+      .map(
+        (data, index) => `
+        <div>
+          <h2>${data.name}</h2>
+          <img class ="indexPokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg"></img>
+          <button class="seePokemon" data-index=${index + 1}>comments</button>
+        </diV>
+      `,
+      )
+      .join('');
+  };
 
-export default createHome;
+  export default createHome;
 
 // <template id="template-grid-item">
 //   <div class="grid-item">
