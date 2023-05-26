296f45676b81dcaf5f828d378908c0e31b3bb1e3
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-25 18:03:10 -0600

    Update style.css

--- src/modules/home.js
+++ src/modules/home.js
@@ -15,16 +15,12 @@ const createHome = async () => {
   }
 };
 
-const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hrqIdiXTh94rmLQMrXcG/likes/');
-const datalikes = await response.json();
-
 const main = document.querySelector('header');
 const list = document.createElement('div');
 list.classList.add('pokeGroup');
 main.insertAdjacentElement('afterEnd', list);
 
 const displayScores = (data) => {
-  const like = datalikes[data.id - 1].likes;
   list.innerHTML = data.map(
     (data, index) => `
       <div class="grid-item">
@@ -32,9 +28,9 @@ const displayScores = (data) => {
           <div class="description">
           <h2>${data.name}</h2>
           <div class="likes">
-            <i class="fa-regular fa-heart like" id=${data.id}></i>
+            <i class="fa-regular fa-heart fa-lg like" id=${data.id}></i>
             <div class="count-likes">
-              <p>${like}</p>
+              <p>##</p>
               <p>likes</p>
             </div>
           </div>
