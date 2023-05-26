9abd5ea273b932308bacf2a73db58540ff8292b3
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-25 19:35:41 -0600

    Fix issue logo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './style.css';
-import './assets/logo.png';
+import logopokemon from './assets/logo.png';
 import getPokeInfo from './modules/popup.js';
 import { createHome } from './modules/home.js';
 import openCard from './modules/reservePokemon.js';
 import likes from './modules/likes.js';
+
+const logo = document.querySelector('.logo');
+logo.src = logopokemon;
 
 document.addEventListener('DOMContentLoaded', async () => {
   getPokeInfo();
